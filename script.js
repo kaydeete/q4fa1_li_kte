5d0228911e899d93c3860b31e9fdb114bbccde14
kaydeedurant <143239699+kaydeete@users.noreply.github.com>
2024-04-11 19:09:56 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+var br = "</br>";
+
+function info()
+{
+    var name = document.getElementById("name").value;
+    var uname = document.getElementById("username").value;
+    var gender = document.getElementById("gender").value;
+    var pword = document.getElementById("password").value;
+    var email = document.getElementById("email").value;
+    var loc = document.getElementById("address").value;
+    var abt = document.getElementById("infouser").value;
+
+    document.getElementById("outputdata").innerHTML =
+    "Name: " + name + br +
+    "Username: " + uname + br +
+    "Gender: " + gender + br +
+    "Password: " + pword + br +
+    "Email: " + email + br +
+    "Address: " + loc + br +
+    "About the User: " + abt + br;
+
+    return false;
+}
+
+function blurT()
+{
+    var inputE = document.getElementsByTagName("input");
+
+    for (var i = 0; i < inputE.length; i++) {
+        if (inputE[i].value == "") {
+            inputE[i].style.borderColor = "red";
+        } else {
+            inputE[i].style.borderColor = "green";
+        }
+    }
+}
+
+function focusT(element)
+{
+    element.style.borderColor = "green";
+}
+
+function reset()
+{
+    document.getElementById("signup").reset();
+    var inputE = document.getElementsByTagName("input");
+    var textE = document.getElementsByTagName("textarea");
+    for (var i = 0; i < inputE.length; i++) {
+        inputE[i].style.borderColor = "black";
+    }
+    for (var i = 0; i < textE.length; i++) {
+        textE[i].style.borderColor = "black";
+    }
+    document.getElementById("outputdata").innerHTML = '';
+    return false;
+}
